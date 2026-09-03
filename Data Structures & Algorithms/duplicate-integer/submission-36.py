b58a632a245fd39f03bb0f9b300e18cb4e10caf7
DavidChan115 <159612070+DavidChan115@users.noreply.github.com>
2026-09-04 00:14:12 +0800

    Add: duplicate-integer - submission-36

---
+++ Data Structures & Algorithms/duplicate-integer/submission-36.py
@@ -0,0 +1,10 @@
+class Solution:
+    def hasDuplicate(self, nums: List[int]) -> bool:
+        return len(set(nums)) < len(nums)
+
+# set(xx) 可以就咁將 係list嘅xx 轉做 set
+
+# set個特性係會自動remove duplicate, 所以如果set length < list length, 等於有duplicate俾set自動remove左
+
+# Time complexity: O(n) because the dominant operation—building the set from the list—requires visiting each of the n elements once
+# Space complexity: set = O(n)
